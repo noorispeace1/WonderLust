57ca59abe0d014c697edd3baca9c0fbcd8c8d3cb
Your Name <ahosanulhaquenoor@gmail.com>
2026-05-15 22:10:30 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,154 @@
+const dns = require("node:dns");
+dns.setServers(["1.1.1.1", "8.8.8.8"]);
+
+
+const express = require("express");
+const dontenv = require("dotenv");
+const cors = require("cors");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+const { createRemoteJWKSet, jwtVerify } = require("jose-cjs");
+dontenv.config();
+
+const uri = process.env.MONGODB_URI;
+
+const app = express();
+const PORT = process.env.PORT;
+
+app.use(cors());
+app.use(express.json());
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+const JWKS = createRemoteJWKSet(
+new URL(`${process.env.CLIENT_URL}/api/auth/jwks`)
+)
+
+const verifyToken = async(req,res,next)=>{
+  const authHeader = req?.headers.authorization;
+  if(!authHeader){
+    return res.status(401).json({message:
+      "unauthorized"
+    });
+  }
+const token = authHeader.split(" ")[1]
+if(!token){
+    return res.status(401).json({message:
+      "unauthorized"
+});}
+
+try {
+  const {payload} = await jwtVerify(token,JWKS)
+console.log(payload);
+next()
+} catch (error) {
+return res.status(403).json({message: "Forbidden"});  
+}
+  next()
+}
+async function run() {
+  try {
+    // await client.connect();
+
+    const db = client.db("wanderlust");
+    const destinationCollection = db.collection("destinations");
+    const bookingCollection = db.collection("bookings");
+
+    app.get("/destination", async (req, res) => {
+      const result = await destinationCollection.find().toArray();
+      res.json(result);
+    });
+
+    app.post("/destination", async (req, res) => {
+      const destinationData = req.body;
+      console.log(destinationData);
+      const result = await destinationCollection.insertOne(destinationData);
+
+      res.json(result);
+    });
+
+    app.get("/destination/:id", 
+    
+       async (req, res) => {
+      const { id } = req.params;
+
+      const result = await destinationCollection.findOne({
+        _id: new ObjectId(id),
+      });
+
+      res.json(result);
+    });
+
+    app.patch("/destination/:id", async (req, res) => {
+      const { id } = req.params;
+      const updatedData = req.body;
+      console.log(updatedData);
+
+      const result = await destinationCollection.updateOne(
+        { _id: new ObjectId(id) },
+        { $set: updatedData },
+      );
+
+      res.json(result);
+    });
+
+    app.delete("/destination/:id", async (req, res) => {
+      const { id } = req.params;
+      const result = await destinationCollection.deleteOne({
+        _id: new ObjectId(id),
+      });
+      res.json(result);
+    });
+
+    app.get("/booking/:userId", async (req, res) => {
+      const { userId } = req.params;
+
+      const result = await bookingCollection.find({ userId: userId }).toArray();
+
+      res.json(result)
+    });
+
+    app.post("/booking", async (req, res) => {
+      const bookingData = req.body;
+      // console.log(bookingData);
+      const result = await bookingCollection.insertOne(bookingData);
+      res.json(result);
+    });
+    app.post("/destinations",verifyToken, async (req, res) => {
+      const adddestination = req.body;
+      console.log(adddestination);
+      // console.log(bookingData);
+      const result = await destinationCollection.insertOne(adddestination);
+      res.json(result);
+    });
+
+
+    app.delete('/booking/:bookingId', async (req, res) => {
+      const {bookingId} = req.params;
+      const result = await bookingCollection.deleteOne({_id: new ObjectId(bookingId)})
+
+      res.json(result)
+    })
+
+    // await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!",
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+  res.send("Server is running fine!");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
